b0a7944f54d610305c543f86825e8c78df6290a0
oshalygin <oshalygin@gmail.com>
2017-07-02 16:03:42 -0700

    Add initial code
    - The added source code is just to read in the
      arguments and print out a few details.  This is
      the initial step in getting the repository setup
      with testing and so forth

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	configurationType := flag.String("type", "deployment", "Kubernetes configuration type, eg: deployment, rc, secret")
+	image := flag.String("image", "", "Docker image name")
+	imageTag := flag.String("tag", "", "Docker image tag")
+	filePath := flag.String("file-path", "", "Configuration file location")
+
+	flag.Parse()
+	checkRequiredFlags(configurationType, image, imageTag, filePath)
+
+	fmt.Printf("type: %s\n", *configurationType)
+	fmt.Printf("image: %s\n", *image)
+	fmt.Printf("tag: %s\n", *imageTag)
+	fmt.Printf("file-path: %s\n", *filePath)
+}
