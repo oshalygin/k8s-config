4f5b884fc0ceeb8ad727894e0c1b95f53f2c446a
oshalygin <oshalygin@gmail.com>
2017-07-02 21:31:01 -0700

    Add model comments
    - This commit adds comments to all exported types
      in the deployment model.
    - This commit also restricts golint to only run
      on this package and not any dependencies

--- models/deployment.go
+++ models/deployment.go
@@ -1,24 +1,28 @@
 package models
 
+// Deployment provides declarative updates for Pods and ReplicaSets (the next-generation ReplicationController).
 type Deployment struct {
-	ApiVersion string
+	APIVersion string
 	Kind       string
 	MetaData
 	Spec
 }
 
+// MetaData field
 type MetaData struct {
 	Name      string
 	Namespace string
 	Labels
 }
 
+// Labels represents arbitrary labels that can be attached to a kubernetes type
 type Labels struct {
 	Version string
 	Date    string
 	App     string
 }
 
+// Spec represents the Deployment specification
 type Spec struct {
 	Replicas             int
 	RevisionHistoryLimit int
@@ -26,25 +30,30 @@ type Spec struct {
 	Template
 }
 
+// Strategy represents the way in which updates occur
 type Strategy struct {
 	RollingUpdate
 	Type string
 }
 
+// RollingUpdate describes the rolling update configuration
 type RollingUpdate struct {
 	MaxUnavailable int
 }
 
+// Template represents the deployment template
 type Template struct {
 	MetaData
 	TemplateSpec
 }
 
+// TemplateSpec provides details for all containers and volumes in a pod
 type TemplateSpec struct {
 	Containers []Container
 	Volumes    []PodVolumes
 }
 
+// Container represents the container in the pod
 type Container struct {
 	Name         string
 	Image        string
@@ -52,25 +61,30 @@ type Container struct {
 	Env          []EnvironmentVariable
 }
 
+// ContainerVolumeMounts represents the mounted volumes on a container
 type ContainerVolumeMounts struct {
 	MountPath string
 	Name      string
 }
 
+// EnvironmentVariable describes the environment variable configuration
 type EnvironmentVariable struct {
 	Name      string
 	ValueFrom []EnvironmentValueFrom
 }
 
+// EnvironmentValueFrom represents the scheme in how the environment variable is retrieved
 type EnvironmentValueFrom struct {
 	SecretKeyRef
 }
 
+// SecretKeyRef provides the key value pair of a secret configuration
 type SecretKeyRef struct {
 	Name string
 	Key  string
 }
 
+// PodVolumes provides the pod volume configuration
 type PodVolumes struct {
 	Name     string
 	EmptyDir string
