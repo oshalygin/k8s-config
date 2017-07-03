f8015cf6a636ac30e8dca27c9a9c5e2a3f2d5016
oshalygin <oshalygin@gmail.com>
2017-07-02 21:45:13 -0700

    Add base logic to unmarshal a yaml

--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import (
 	"fmt"
 	"os"
 
+	"io/ioutil"
+
 	"github.com/fatih/color"
+	"github.com/oshalygin/k8s-config/models"
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
+	configurationFile := models.Deployment{}
+
 	configurationType := flag.String("type", "deployment", "Kubernetes configuration type, eg: deployment, rc, secret")
 	image := flag.String("image", "", "Docker image name")
 	imageTag := flag.String("tag", "", "Docker image tag")
@@ -16,7 +22,19 @@ func main() {
 
 	flag.Parse()
 
-	err := checkRequiredFlags(*configurationType, *image, *imageTag, *filePath)
+	file, err := ioutil.ReadFile("./test-files/deployment.yaml")
+	if err != nil {
+		color.Red("Error: %v", err)
+		os.Exit(1)
+	}
+
+	err = yaml.Unmarshal(file, &configurationFile)
+	if err != nil {
+		color.Red("Error: %v", err)
+		os.Exit(1)
+	}
+
+	err = checkRequiredFlags(*configurationType, *image, *imageTag, *filePath)
 	if err != nil {
 		color.Red("Error: %v", err)
 		color.Black("--------------------")
