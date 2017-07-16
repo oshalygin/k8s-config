0696bb75c44897b632d19f38cdbd28076af4c9b4
oshalygin <oshalygin@gmail.com>
2017-07-15 22:42:04 -0700

    Update deployment script
    - Addition of deployment script error handling to
      catch any errors and throw the appropriate status
      code

--- .travis.d/publish.sh
+++ .travis.d/publish.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #
 # This script builds the utility targeting the Travis sudo build environment
+set -o nounset
+set -o errexit -o errtrace
 
 # Golang architecture and operating system documentation
 # https://golang.org/doc/install/source#environment
